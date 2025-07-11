3f45f4a641393b4a74ae557f6619008f87b63fbf
Fellipe-Borsato-Pixelti <fellipe.borsato@pixelti.com.br>
2025-07-11 17:02:17 -0300

    organização dos codigos

--- src/components/btn/index.js
+++ src/components/btn/index.js
@@ -1,155 +1,234 @@
-import React from 'react'
+import React from "react";
 
-const Teclado = ({ adicionarTexto, limparTexto, calcularResultado, apagarRespostaAnterior, mostrarImagem, apagarCaractere }) => {
+const Teclado = ({
+  adicionarTexto,
+  limparTexto,
+  calcularResultado,
+  apagarRespostaAnterior,
+  mostrarImagem,
+  apagarCaractere,
+}) => {
   return (
     <>
-      <div className='flex-col justify-center items-center p[16px]'>
-        <div className='grid grid-cols-4 gap-x-[14px] gap-y-[14px] w-fit p-[20px] '> 
-          <button 
+      <div className="flex-col justify-center items-center p[16px]">
+        <div className="grid grid-cols-4 gap-x-[14px] gap-y-[14px] w-fit p-[20px] ">
+          <button
             onClick={mostrarImagem}
-            className='h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]'>Mito</button>
+            className="h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]"
+          >
+            Mito
+          </button>
 
-          <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('√')
-        }}className='h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]'>Raiz
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("√");
+            }}
+            className="h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]"
+          >
+            Raiz
+          </button>
 
-          <button  onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('^')
-        }}
-        className='h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]'>Elevado</button>
-          <button onClick={() => {{apagarCaractere()}}}
-          className='h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]'>Apagar</button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("^");
+            }}
+            className="h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]"
+          >
+            Elevado
+          </button>
+          <button
+            onClick={() => {
+              {
+                apagarCaractere();
+              }
+            }}
+            className="h-[27px] w-[75px] bg-[#9ACD32] text-black font-bold rounded-[16px]"
+          >
+            Apagar
+          </button>
         </div>
-        <div className= 'grid grid-cols-4 gap-x-[14px] gap-y-[14px] w-fit p-[20px] bg-[#1e1e1e] rounded-[16px] shadow-[0-0-12px-rgba(0,0,0,0.6)]'>
+        <div className="grid grid-cols-4 gap-x-[14px] gap-y-[14px] w-fit p-[20px] bg-[#1e1e1e] rounded-[16px] shadow-[0-0-12px-rgba(0,0,0,0.6)]">
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("9");
+            }}
+            style={estiloBotao}
+          >
+            9
+          </button>
 
-          <button onClick={() => {
-           apagarRespostaAnterior(); 
-           adicionarTexto('9');
-        }}style={estiloBotao}>9
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("8");
+            }}
+            style={estiloBotao}
+          >
+            8
+          </button>
 
-          <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('8')
-        }}style={estiloBotao}>8
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("7");
+            }}
+            style={estiloBotao}
+          >
+            7
+          </button>
 
-          <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('7')
-        }}style={estiloBotao}>7
-        </button>
+          <button
+            onClick={() => {
+              adicionarTexto("+");
+            }}
+            style={estiloBotaoOperacao}
+          >
+            +
+          </button>
 
-          <button onClick={() => {
-            adicionarTexto('+');
-        }}style={estiloBotaoOperacao}>+
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("6");
+            }}
+            style={estiloBotao}
+          >
+            6
+          </button>
 
-          <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('6')
-        }}style={estiloBotao}>6
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("5");
+            }}
+            style={estiloBotao}
+          >
+            5
+          </button>
 
-          <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('5')
-        }}style={estiloBotao}>5
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("4");
+            }}
+            style={estiloBotao}
+          >
+            4
+          </button>
 
-        <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('4')
-        }}style={estiloBotao}>4
-        </button>
+          <button
+            onClick={() => {
+              adicionarTexto("-");
+            }}
+            style={estiloBotaoOperacao}
+          >
+            -
+          </button>
 
-        <button onClick={() => {
-          adicionarTexto('-');
-        }}style={estiloBotaoOperacao}>-
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("3");
+            }}
+            style={estiloBotao}
+          >
+            3
+          </button>
 
-        <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('3')
-        }}style={estiloBotao}>3
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("2");
+            }}
+            style={estiloBotao}
+          >
+            2
+          </button>
 
-        <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('2')
-        }}style={estiloBotao}>2
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("1");
+            }}
+            style={estiloBotao}
+          >
+            1
+          </button>
 
-        <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('1')
-        }}style={estiloBotao}>1
-        </button>
+          <button
+            onClick={() => {
+              adicionarTexto("*");
+            }}
+            style={estiloBotaoOperacao}
+          >
+            x
+          </button>
 
-        <button onClick={() => {
-        adicionarTexto('*');
-        }}style={estiloBotaoOperacao}>x
-        </button>
+          <button onClick={limparTexto} style={estiloBotaoEspecial}>
+            C
+          </button>
 
-        <button onClick= {limparTexto} 
-            style={estiloBotaoEspecial}>C
-        </button>
+          <button
+            onClick={() => {
+              apagarRespostaAnterior();
+              adicionarTexto("0");
+            }}
+            style={estiloBotao}
+          >
+            0
+          </button>
 
-        <button onClick={() => {
-            apagarRespostaAnterior();
-            adicionarTexto('0')
-        }}style={estiloBotao}>0
-        </button>
+          <button onClick={calcularResultado} style={estiloBotaoOperacao}>
+            =
+          </button>
 
-        <button onClick={calcularResultado}
-            style={estiloBotaoOperacao}>=
-        </button>
-
-        <button onClick={() => {
-        adicionarTexto('/');
-        }}style={estiloBotaoOperacao}>÷
-        </button>
-
+          <button
+            onClick={() => {
+              adicionarTexto("/");
+            }}
+            style={estiloBotaoOperacao}
+          >
+            ÷
+          </button>
         </div>
-        
       </div>
     </>
   );
-}
+};
 
-const estiloBotao = { 
-  width: '75px',
-  height: '75px',
-  fontSize: '22px',
-  fontWeight: 'bold',
-  backgroundColor: '#ADFF2F',
-  color: '#000',
-  borderRadius: '10px',
-  border: 'none',
-  boxShadow: '0 2px 6px rgba(0,0,0,0.3)',
-  cursor: 'pointer',
+const estiloBotao = {
+  width: "75px",
+  height: "75px",
+  fontSize: "22px",
+  fontWeight: "bold",
+  backgroundColor: "#ADFF2F",
+  color: "#000",
+  borderRadius: "10px",
+  border: "none",
+  boxShadow: "0 2px 6px rgba(0,0,0,0.3)",
+  cursor: "pointer",
 };
 
 const estiloBotaoOperacao = {
-  width: '75px',
-  height: '75px',
-  fontSize: '35px',
-  fontWeight: 'bold',
-  backgroundColor: '#9ACD32',
-  color: '#000',
-  borderRadius: '10px',
-  border: 'none',
-  boxShadow: '0 2px 6px rgba(0,0,0,0.3)',
-  cursor: 'pointer',
+  width: "75px",
+  height: "75px",
+  fontSize: "35px",
+  fontWeight: "bold",
+  backgroundColor: "#9ACD32",
+  color: "#000",
+  borderRadius: "10px",
+  border: "none",
+  boxShadow: "0 2px 6px rgba(0,0,0,0.3)",
+  cursor: "pointer",
 };
 
 const estiloBotaoEspecial = {
   ...estiloBotao,
-  backgroundColor: '#FF0000',
-  color: '#fff'
+  backgroundColor: "#FF0000",
+  color: "#fff",
 };
 
 export default Teclado;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
-"use client"
+"use client";
 
-import React, { useState, useRef } from 'react'
-import Teclado from 'C:/Users/pixel/Desktop/estudo/calculadora-tailwind/my-app/src/components/btn'
-
+import React, { useState, useRef } from "react";
+import Teclado from "C:/Users/pixel/Desktop/estudo/calculadora-tailwind/my-app/src/components/btn";
 
 function Home() {
   const textareaRef = useRef(null);
-  
-  const [fezConta, setfezConta] = useState("false")
-  const [deve_Aparecer_Imagem, set_Deve_Aparecer_Imagem] = useState(false)
-  const [texto, setTexto] = useState('');
+
+  const [fezConta, setfezConta] = useState("false");
+  const [deve_Aparecer_Imagem, set_Deve_Aparecer_Imagem] = useState(false);
+  const [texto, setTexto] = useState("");
 
   //---------------------------------------------------------
   const focarTextarea = () => {
@@ -17,17 +16,17 @@ function Home() {
   };
 
   const adicionarTexto = (valor) => {
-    setTexto(prev => prev + valor);
+    setTexto((prev) => prev + valor);
     focarTextarea();
   };
 
   const limparTexto = () => {
-    setTexto('');
+    setTexto("");
     focarTextarea();
   };
-//-----------------------------------------------------------
+  //-----------------------------------------------------------
   const calcularResultado = () => {
-    setfezConta("true")
+    setfezConta("true");
     try {
       const resultado = eval(texto);
       setTexto(resultado.toString());
@@ -36,60 +35,59 @@ function Home() {
     }
     focarTextarea();
   };
-//-----------------------------------------------------------
-const apagarRespostaAnterior = () => {
-  if (fezConta === "true") {
-    setTexto('');
-    setfezConta("false");
-  }
-  focarTextarea();
-};
-
-//-----------------------------------------------------------
-  const apagarCaractere = (valor) => {
-    setTexto(prev => prev.slice(0, -1));
+  //-----------------------------------------------------------
+  const apagarRespostaAnterior = () => {
+    if (fezConta === "true") {
+      setTexto("");
+      setfezConta("false");
+    }
     focarTextarea();
   };
 
-//-----------------------------------------------------------
+  //-----------------------------------------------------------
+  const apagarCaractere = (valor) => {
+    setTexto((prev) => prev.slice(0, -1));
+    focarTextarea();
+  };
 
-const mostrarImagem = () => {
-  set_Deve_Aparecer_Imagem(prev => !prev);
-};
+  //-----------------------------------------------------------
 
-  return(
+  const mostrarImagem = () => {
+    set_Deve_Aparecer_Imagem((prev) => !prev);
+  };
+
+  return (
     <>
-    <div className='grid grid-cols-2 bg-black'>
-      <div className=' flex justify-center items-center h-screen flex-col bg-black'>
-        <div className= 'bg-[#1f1f1f] border-[3px] border-[#ADFF2F] rounded-[16px] p-6 shadow-[0_0_15px_rgb(0,128,0)]'>
-          <textarea 
-            ref={textareaRef}
-            value={texto}
-            onKeyDown={e => e.preventDefault()} // Bloqueia teclado físico
-            className='overflow-hidden whitespace-nowrap resize-none text-right text-[40px] font-bold bg-[#2c2c2c] text-[#e0ffe0] border-[2px] border-[#ADFF2F] rounded-[10px] outline-none p-[10px] shadow-[inset_0_0_10px_rgba(255,255,255)] w-[380px] h-[80px] mb-[10px]'
-            style={{ fontFamily: 'Calculadora' }}
-          />
-          <Teclado 
-            adicionarTexto={adicionarTexto} 
-            limparTexto={limparTexto} 
-            calcularResultado={calcularResultado} 
-            apagarRespostaAnterior={apagarRespostaAnterior}
-            mostrarImagem={mostrarImagem} 
-            apagarCaractere={apagarCaractere}
-          />
+      <div className="grid grid-cols-2 bg-black">
+        <div className=" flex justify-center items-center h-screen flex-col bg-black">
+          <div className="bg-[#1f1f1f] border-[3px] border-[#ADFF2F] rounded-[16px] p-6 shadow-[0_0_15px_rgb(0,128,0)]">
+            <textarea
+              ref={textareaRef}
+              value={texto}
+              onKeyDown={(e) => e.preventDefault()} // Bloqueia teclado físico
+              className="overflow-hidden whitespace-nowrap resize-none text-right text-[40px] font-bold bg-[#2c2c2c] text-[#e0ffe0] border-[2px] border-[#ADFF2F] rounded-[10px] outline-none p-[10px] shadow-[inset_0_0_10px_rgba(255,255,255)] w-[380px] h-[80px] mb-[10px]"
+              style={{ fontFamily: "Calculadora" }}
+            />
+            <Teclado
+              adicionarTexto={adicionarTexto}
+              limparTexto={limparTexto}
+              calcularResultado={calcularResultado}
+              apagarRespostaAnterior={apagarRespostaAnterior}
+              mostrarImagem={mostrarImagem}
+              apagarCaractere={apagarCaractere}
+            />
+          </div>
+        </div>
+        <div className="flex justify-center items-center h-screen">
+          {deve_Aparecer_Imagem && (
+            <img
+              src="/bolsonaro.jpg"
+              alt="foto do bolsonaro"
+              className="mt-4 w-[900px] mx-auto"
+            />
+          )}
         </div>
       </div>
-      <div className= 'flex justify-center items-center h-screen'>
-          {deve_Aparecer_Imagem && (
-          <img 
-          src="/bolsonaro.jpg"
-          alt="foto do bolsonaro"
-          className="mt-4 w-[900px] mx-auto"
-        />
-      )}
-
-      </div>
-    </div>
     </>
   );
 }
