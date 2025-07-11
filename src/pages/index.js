28091a53f244469a755d1f1c1bf420932d0cda42
Fellipe-Borsato-Pixelti <fellipe.borsato@pixelti.com.br>
2025-07-11 15:00:11 -0300

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,115 +1,97 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+"use client"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import React, { useState, useRef } from 'react'
+import Teclado from 'C:/Users/pixel/Desktop/estudo/calculadora-tailwind/my-app/src/components/btn'
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+function Home() {
+  const textareaRef = useRef(null);
+  
+  const [fezConta, setfezConta] = useState("false")
+  const [deve_Aparecer_Imagem, set_Deve_Aparecer_Imagem] = useState(false)
+  const [texto, setTexto] = useState('');
+
+  //---------------------------------------------------------
+  const focarTextarea = () => {
+    if (textareaRef.current) textareaRef.current.focus();
+  };
+
+  const adicionarTexto = (valor) => {
+    setTexto(prev => prev + valor);
+    focarTextarea();
+  };
+
+  const limparTexto = () => {
+    setTexto('');
+    focarTextarea();
+  };
+//-----------------------------------------------------------
+  const calcularResultado = () => {
+    setfezConta("true")
+    try {
+      const resultado = eval(texto);
+      setTexto(resultado.toString());
+    } catch (err) {
+      setTexto("Erro");
+    }
+    focarTextarea();
+  };
+//-----------------------------------------------------------
+const apagarRespostaAnterior = () => {
+  if (fezConta === "true") {
+    setTexto('');
+    setfezConta("false");
+  }
+  focarTextarea();
+};
+
+//-----------------------------------------------------------
+  const apagarCaractere = (valor) => {
+    setTexto(prev => prev.slice(0, -1));
+    focarTextarea();
+  };
+
+//-----------------------------------------------------------
+
+const mostrarImagem = () => {
+  set_Deve_Aparecer_Imagem(prev => !prev);
+};
+
+  return(
+    <>
+    <div className='grid grid-cols-2 bg-black'>
+      <div className=' flex justify-center items-center h-screen flex-col bg-black'>
+        <div className= 'bg-[#1f1f1f] border-[3px] border-[#ADFF2F] rounded-[16px] p-6 shadow-[0_0_15px_rgb(0,128,0)]'>
+          <textarea 
+            ref={textareaRef}
+            value={texto}
+            onKeyDown={e => e.preventDefault()} // Bloqueia teclado físico
+            className='overflow-hidden whitespace-nowrap resize-none text-right text-[40px] font-bold bg-[#2c2c2c] text-[#e0ffe0] border-[2px] border-[#ADFF2F] rounded-[10px] outline-none p-[10px] shadow-[inset_0_0_10px_rgba(255,255,255)] w-[380px] h-[80px] mb-[10px]'
+            style={{ fontFamily: 'Calculadora' }}
+          />
+          <Teclado 
+            adicionarTexto={adicionarTexto} 
+            limparTexto={limparTexto} 
+            calcularResultado={calcularResultado} 
+            apagarRespostaAnterior={apagarRespostaAnterior}
+            mostrarImagem={mostrarImagem} 
+            apagarCaractere={apagarCaractere}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className= 'flex justify-center items-center h-screen'>
+          {deve_Aparecer_Imagem && (
+          <img 
+          src="/bolsonaro.jpg"
+          alt="foto do bolsonaro"
+          className="mt-4 w-[900px] mx-auto"
+        />
+      )}
+
+      </div>
     </div>
+    </>
   );
 }
+
+export default Home;
